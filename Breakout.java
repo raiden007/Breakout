1e51cfa1113ac4e5e710391b5f2eb5bd257579c8
S <s>
2015-02-13 17:22:07 +0200

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -136,7 +136,7 @@ public class Breakout extends GraphicsProgram {
 		vx = rgen.nextDouble(1.0, 3.0);
 		if (rgen.nextBoolean(0.5)) vx = -vx;
 		vy += 3;
-		int speed = 50;
+		int speed = 10;
 		int lives = NTURNS;
 		
 		// moving the ball
